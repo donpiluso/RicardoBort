e9eeb94ccb1c8a4d4b4a1a5b54adf514628c145c
donpiluso <donpiluso@gmail.com>
2016-01-11 14:51:26 -0300

    Privados sin @rickybot && test deploy.php

--- lib/message.php
+++ lib/message.php
@@ -73,7 +73,7 @@ function processMessage($message,$update) {
 	} elseif(stripos($text, "@Rickybort_bot")!==false || stripos($text, "ricky")!==false) { // NO ES UN COMANDO
 		if(stripos($text, "hola bot") === 0 || stripos($text, "hola") === 0 || stripos($text, "Hola comandante") === 0){
 			reply_message($message, "Hola " . $nombre_saludo);
-		}elseif($text !="" && stripos($text, "@Rickybort_bot")!==false){
+		}elseif(($text !="" && stripos($text, "@Rickybort_bot")!==false) || $message["chat"]["type"]=="private"){
 			reply_message($message, get_random_message('respuestas', "Algo Salió mal ".$nombre_saludo));
 		} elseif($message["new_chat_participant"]){
 			reply_message($message, 'HOLA '.strtoupper($message['new_chat_participant']['first_name'])." BIENVENIDO A MAIAMEEEE MI AMOR..\n\nESTE GRUPO ESTA EN LO MAS ALTO PORQUE ESTOY YO CON MI ROLLS ROYCE\nADEMAS SON TODOS UNOS GENIOS Y HACEN LOS MEJORES STICKERS\nSI SOS TREMENDO PUTO LOS JUEVES HAY PIJAS Y SINO TODOS LOS DIAS HAY TETAS\n\nOJALÁ LA VIDA TE SONRÍA COMO ME SONRÍE A MI Y PUEDAS DISFRUTAR LA VIDA COMO LO HAGO YO.BESO");
